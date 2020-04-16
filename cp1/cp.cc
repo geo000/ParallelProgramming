e9002cd440d802a4ad3ef785796104ed97a50559
Göncz Levente <gonczl1@spinelli.org.aalto.fi>
2020-04-16 15:12:00 +0300

    declaration changes

--- cp1/cp.cc
+++ cp1/cp.cc
@@ -5,39 +5,37 @@
 #include <numeric>
 
 void correlate(int ny, int nx, const float* data, float* result) {
-    int row, column, i, j, k;
-    double x, mean, sum, square_sum;
     std::vector<double> interm_data(nx * ny);
-    for (row = 0; row < ny ; row ++)
+    for (int row = 0; row < ny ; row ++)
     {
-        sum = 0;
-        for (column = 0; column < nx; column ++)
+        double sum = 0;
+        for (int column = 0; column < nx; column ++)
         {
             sum += data[column + row * nx];
         }
-        mean = sum / nx;
-        square_sum = 0;
-        for (column = 0; column < nx; column ++)
+        double mean = sum / nx;
+        double square_sum = 0;
+        for (int column = 0; column < nx; column ++)
         {
-            x = data[column + row * nx] - mean;
+            double x = data[column + row * nx] - mean;
             interm_data[column + row * nx] = x;
             square_sum += x * x;
         }
-        square_sum =  sqrt(square_sum);
-        for(column = 0; column < nx; column++) {
+        square_sum = sqrt(square_sum);
+        for(int column = 0; column < nx; column++) {
             interm_data[column + row * nx] /= square_sum;
         }
     }
-    for (i = 0; i < ny ; i++)
+    for (int i = 0; i < ny ; i++)
     {
-        for (j = i; j < ny; j++) 
+        for (int j = i; j < ny; j++) 
         {
-            x = 0;
-            for (k = 0; k < nx; k++)
+            double sumx = 0;
+            for (int k = 0; k < nx; k++)
             {
-                x +=  interm_data[k + i * nx] * interm_data[k + j * nx];
+                sumx += interm_data[k + i * nx] * interm_data[k + j * nx];
             }
-            result[j + i * ny] = (float)x;
+            result[j + i * ny] = (float) sumx;
         }
     }
 }
