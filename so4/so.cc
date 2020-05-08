f340b079a1743fc1a6745d7ea61e50cdc90233af
Göncz Levente <gonczl1@pelikaani.org.aalto.fi>
2020-05-08 13:28:30 +0300

    divide and conquer - 5points

--- so4/so.cc
+++ so4/so.cc
@@ -1,7 +1,64 @@
 #include "so.h"
 #include <algorithm>
+#include "math.h"
+#include <stdio.h>
+#include <vector>
+#include <numeric>
+#include <omp.h>
+#include<iostream>
+using namespace std;
 
-void psort(int n, data_t* data) {
-    // FIXME: make this more efficient with parallelism
-    std::sort(data, data + n);
+int nearestpwr2 (int x)
+{
+    x = x | (x >> 1);
+    x = x | (x >> 2);
+    x = x | (x >> 4);
+    x = x | (x >> 8);
+    x = x | (x >> 16);
+    return x - (x >> 1);
+}
+
+void print(data_t* data, int n)
+{
+	for(int i = 0; i < n; i++)
+    {
+		cout << data[i] << " ";
+	    cout << endl;
+    }
+}
+void mergeSort(data_t* data, int input_size) 
+{
+    int max_thread = omp_get_max_threads();
+    int round_down = nearestpwr2(max_thread);
+    if (round_down < 2)
+    {
+        sort(data, data + input_size);
+        return;
+    }
+
+    int block_size = input_size / round_down;
+    if(input_size % round_down) block_size++;
+
+    #pragma omp parallel num_threads(round_down)
+    {
+        int thread_num = omp_get_thread_num();
+        sort(data + min(thread_num * block_size, input_size), data + min((thread_num+1) * block_size, input_size));
+    }
+    round_down >>= 1;
+    do
+    {   
+        #pragma omp parallel num_threads(round_down)
+        {
+            int thread_num = omp_get_thread_num() << 1;
+            inplace_merge(data + thread_num * block_size, data + min((thread_num+1) * block_size, input_size),
+                          data + min((thread_num+2) * block_size, input_size));
+        }
+    block_size <<= 1;
+    round_down >>= 1;
+    }while(round_down > 0);
+}
+
+void psort(int n, data_t* data) 
+{
+    mergeSort(data, n);
 }
