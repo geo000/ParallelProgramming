fa469468e6cc851bcdbd348724c992af9765b57b
Göncz Levente <gonczl1@peridootti.org.aalto.fi>
2020-05-13 16:14:16 +0300

    for debugging

--- cp5/cp.cu
+++ cp5/cp.cu
@@ -28,28 +28,42 @@ static inline void check(cudaError_t err, const char* context) {
 
 #define CHECK(x) check(x, #x)
 
-__global__ void mykernel(int ny, int nx, float* norm_data, float* norm_data_transpose, float* result) {
+__global__ void mykernel(int ny, int nx, int nn, float* norm_data, float* norm_data_transpose, float* result) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
-    if (i >= ny || j >= ny || j < i)
+    if (i >= nn || j >= nn || j < i)
         return;
     float sumx = 0;
-    for (int k = 0; k < nx; k++){
-        sumx += norm_data_transpose[i + k * ny] * norm_data[k + j * nx];
+    for (int k = 0; k < nn; k++){
+        sumx += norm_data_transpose[i + k * nn] * norm_data[k + j * nn];
     }
-    result[j + i * ny] = sumx;
+    if (i < ny && j < ny)
+    {
+        result[j + i * ny] = sumx;
+    }
+
 }
 
-__global__ void mykernel_transpose(int ny, int nx, float* norm_data, float* norm_data_transpose) {
+__global__ void mykernel_transpose(int ny, int nx, int nn, float* norm_data, float* norm_data_transpose) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
-    if (i >= ny || j >= nx)
+    if (i >= nn || j >= nn)
         return;
-    norm_data_transpose[i + j * ny] = norm_data[j + i * nx];
+    norm_data_transpose[i + j * nn] = norm_data[j + i * nn];
 }
 
 void correlate(int ny, int nx, const float* data, float* result) {
-    float *interm_data= (float *)malloc(sizeof(float)*ny*nx);
+    int nn = 0;
+    if (ny >= nx)
+    {
+        nn = roundup(ny, 64);
+    }
+    else
+    {
+        nn = roundup(nx, 64);
+    }
+
+    float *interm_data= (float *)malloc(sizeof(float) * nn * nn);
     for (int row = 0; row < ny ; row ++)
     {
         float sum = 0;
@@ -62,12 +76,20 @@ void correlate(int ny, int nx, const float* data, float* result) {
         for (int column = 0; column < nx; column ++)
         {
             float x = data[column + row * nx] - mean;
-            interm_data[column + row * nx] = x;
+            interm_data[column + row * nn] = x;
             square_sum += x * x;
         }
         square_sum = sqrt(square_sum);
         for(int column = 0; column < nx; column++) {
-            interm_data[column + row * nx] /= square_sum;
+            interm_data[column + row * nn] /= square_sum;
+        }
+    }
+    
+    for (int row = ny; row < nn ; row ++)
+    {
+        for (int column = nx; column < nn; column ++)
+        {
+            interm_data[column + row * nn] = 0;
         }
     }
 
@@ -75,25 +97,25 @@ void correlate(int ny, int nx, const float* data, float* result) {
     float* dGPU = NULL;
     float* dGPU_transpose = NULL;
     float* rGPU = NULL;
-    CHECK(cudaMalloc((void**)&dGPU, nx * ny * sizeof(float)));
-    CHECK(cudaMalloc((void**)&dGPU_transpose, nx * ny * sizeof(float)));
+    CHECK(cudaMalloc((void**)&dGPU, nn * nn * sizeof(float)));
+    CHECK(cudaMalloc((void**)&dGPU_transpose, nn * nn * sizeof(float)));
     CHECK(cudaMalloc((void**)&rGPU, ny * ny * sizeof(float)));
 
-    CHECK(cudaMemcpy(dGPU, interm_data, nx * ny * sizeof(float), cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(dGPU, interm_data, nn * nn * sizeof(float), cudaMemcpyHostToDevice));
 
     // Run kernel for transpose
     {
         dim3 dimBlock(32, 32);
-        dim3 dimGrid(divup(ny, dimBlock.x), divup(nx, dimBlock.y));
-        mykernel_transpose<<<dimGrid, dimBlock>>>(ny, nx, dGPU, dGPU_transpose);
+        dim3 dimGrid(divup(nn, dimBlock.x), divup(nn, dimBlock.y));
+        mykernel_transpose<<<dimGrid, dimBlock>>>(ny, nx, nn, dGPU, dGPU_transpose);
         CHECK(cudaGetLastError());
     }
 
     // Run kernel for matrix multiplication
     {
         dim3 dimBlock(32, 32);
-        dim3 dimGrid(divup(ny, dimBlock.x), divup(ny, dimBlock.y));
-        mykernel<<<dimGrid, dimBlock>>>(ny, nx, dGPU, dGPU_transpose, rGPU);
+        dim3 dimGrid(divup(nn, dimBlock.x), divup(nn, dimBlock.y));
+        mykernel<<<dimGrid, dimBlock>>>(ny, nx, nn, dGPU, dGPU_transpose, rGPU);
         CHECK(cudaGetLastError());
     }
 
