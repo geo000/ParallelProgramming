26d95abd18aeca7a572847c52c1885d87431a049
Göncz Levente <gonczl1@kiwi.org.aalto.fi>
2020-05-14 21:36:51 +0300

    only 2 threads for benchmark :(

--- so5/so.cc
+++ so5/so.cc
@@ -7,32 +7,27 @@
 
 void quicksort(data_t* first, data_t* last, int max_thread){
 
-    max_thread = max_thread / 2;
-
-    if (max_thread < 2 || (last-first) < 3)
+    // if number of threads < 2 or number of elements < 3
+    if (max_thread < 2 || (last-first) < 2)
     {
         std::sort(first, last+1);
         return;
     }
+    data_t pivot = *(first + rand() % (last - first));
 
-    data_t pivot = *first;
-
-    // elements < pivot : *it1 = [data + first, it] it is the pivot
-    auto it = std::partition(first, last + 1, [pivot](auto element){ return element < pivot; });
-
-    // elements = pivot : *it1 = [it, it2] it2 is > pivot
-    auto it2 = std::partition(it, last + 1, [pivot](auto element){ return element == pivot; });
-    // elements > pivot : *it1 = [it2, data + last]
-
-
+    // elements < pivot : [first, it] 'it' is the pivot
+    data_t* it = std::partition(first, last + 1, [pivot](data_t element){ return element < pivot; });
+    // elements = pivot : [it, it2] it2 is > pivot
+    data_t* it2 = std::partition(it, last + 1, [pivot](data_t element){ return element == pivot; });
+    // elements > pivot :  [it2, last]
     #pragma omp task
-    quicksort(first, it, max_thread);
+    quicksort(first, it, max_thread / 2);
     #pragma omp task
-    quicksort(it2, last, max_thread);
+    quicksort(it2, last, max_thread / 2);
 }
 
 void psort(int n, data_t* data) {
-    int max_thread = omp_get_max_threads() * 2;
+    int max_thread = omp_get_max_threads();
     #pragma omp parallel
     #pragma omp single
     quicksort(data + 0, data + n-1, max_thread);
