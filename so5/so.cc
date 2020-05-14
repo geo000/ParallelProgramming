b33632c17b9cfe62f845429a22e763eb01ab0f98
Göncz Levente <gonczl1@kiwi.org.aalto.fi>
2020-05-14 22:54:29 +0300

    median 3 points

--- so5/so.cc
+++ so5/so.cc
@@ -5,22 +5,52 @@
 #include <stdio.h>
 #include<iostream> 
 using namespace std;
+data_t middleOfThree(data_t a, data_t b, data_t c) 
+{ 
+    // x is positive if a is greater than b.  
+    // x is negative if b is greater than a. 
+    data_t x = a - b; 
+  
+    data_t y = b - c;  // Similar to x 
+    data_t z = a - c;  // Similar to x and y. 
+  
+    // Checking if b is middle (x and y both 
+    // are positive) 
+    if (x * y > 0) 
+        return b; 
+  
+    // Checking if c is middle (x and z both 
+    // are positive) 
+    else if (x * z > 0) 
+        return c; 
+    else
+        return a; 
+} 
+
 void quicksort(data_t* first, data_t* last, int max_thread){
 
-    // if number of threads < 2 or number of elements <= 2
-    if (max_thread < 2 || (last-first) < 2)
+    // if number of threads < 2 or number of elements <= 1
+    if (max_thread < 2 || (last-first) < 1)
     {
         sort(first, last+1);
         return;
     }
-    data_t pivot = *(last-(last-first)/2);
-    // *(last-(last-first)/2); // time = 2.84
-    //*(first + rand() % (last - first)); // time = 4.8s
-    //max(min(*first,*(first + (last-first)/2)), min(max(*first,*(first + (last-first)/2)),*last)); // time = 3.78s
+    data_t pivot = middleOfThree(*first, *(last-(last-first)/2), *last);
+
+
+    /*
+        PIVOT CHOOSING METHODS
+        <-------------------->
+            1. time = 2.84s - TRIAL AND ERROR#1 -->  *(last-(last-first)/2);
+            2. time = 2.99s - MEDIAN            -->  middleOfThree(*first, *(last-(last-first)/2), *last);
+            3. time = 4.8s  - RANDOM            --> *(first + rand() % (last - first));
+        <-------------------->
+    */
+
 
     // elements < pivot : [first, it] 'it' is the pivot
     data_t* it = partition(first, last + 1, [pivot](data_t element){ return element < pivot; });
-    // elements = pivot : [it, it2] it2 is > pivot
+    // elements == pivot : [it, it2] it2 is > pivot
     data_t* it2 = partition(it, last + 1, [pivot](data_t element){ return element == pivot; });
     // elements > pivot :  [it2, last]
     #pragma omp task
