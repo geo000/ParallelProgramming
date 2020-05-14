c442eda5035a1caa923f785098ff11f2b7ede155
Göncz Levente <gonczl1@kiwi.org.aalto.fi>
2020-05-14 22:11:14 +0300

    trial and error - 3 points

--- so5/so.cc
+++ so5/so.cc
@@ -4,21 +4,24 @@
 #include <omp.h>
 #include <stdio.h>
 #include<iostream> 
-
+using namespace std;
 void quicksort(data_t* first, data_t* last, int max_thread){
 
     // if number of threads < 2 or number of elements <= 2
     if (max_thread < 2 || (last-first) < 2)
     {
-        std::sort(first, last+1);
+        sort(first, last+1);
         return;
     }
-    data_t pivot = *last;
+    data_t pivot = *(last-(last-first)/2);
+    // *(last-(last-first)/2); // time = 2.84
+    //*(first + rand() % (last - first)); // time = 4.8s
+    //max(min(*first,*(first + (last-first)/2)), min(max(*first,*(first + (last-first)/2)),*last)); // time = 3.78s
 
     // elements < pivot : [first, it] 'it' is the pivot
-    data_t* it = std::partition(first, last + 1, [pivot](data_t element){ return element < pivot; });
+    data_t* it = partition(first, last + 1, [pivot](data_t element){ return element < pivot; });
     // elements = pivot : [it, it2] it2 is > pivot
-    data_t* it2 = std::partition(it, last + 1, [pivot](data_t element){ return element == pivot; });
+    data_t* it2 = partition(it, last + 1, [pivot](data_t element){ return element == pivot; });
     // elements > pivot :  [it2, last]
     #pragma omp task
     quicksort(first, it, max_thread / 2);
