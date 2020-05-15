544152a3be13b4ff82f7ef40d2834306dcf5c717
Göncz Levente <gonczl1@sorsa.org.aalto.fi>
2020-05-15 16:48:34 +0300

    with threshold 5 points

--- so5/so.cc
+++ so5/so.cc
@@ -10,17 +10,13 @@ data_t median(data_t first, data_t middle, data_t last)
     data_t x = first - middle; 
     data_t y = middle - last;  
     data_t z = first - last; 
-    if (x * y > 0) 
-        return middle; 
-    else if (x * z > 0) 
-        return last; 
-    else
-        return first; 
+    if (x * y > 0) return middle; 
+    else if (x * z > 0) return last; 
+    else return first; 
 } 
 
-void quicksort(data_t* first, data_t* last, int max_thread){
-    //printf("\nNumber of threads: %d Size: %lu\n", max_thread, last-first + 1);
-    if (max_thread == 1 || (last-first) < 1)
+void quicksort(data_t* first, data_t* last, int threshold){
+    if ((last-first) <= threshold)
     {
         sort(first, last+1);
         return;
@@ -29,19 +25,19 @@ void quicksort(data_t* first, data_t* last, int max_thread){
     data_t* it = partition(first, last + 1, [pivot](data_t element){ return element < pivot; });
     data_t* it2 = partition(it, last + 1, [pivot](data_t element){ return element == pivot; });
     #pragma omp task
-    quicksort(first, it, max_thread / 2);
+    quicksort(first, it, threshold);
     #pragma omp task
-    quicksort(it2, last, max_thread / 2);
+    quicksort(it2, last, threshold);
 }
 
 void psort(int n, data_t* data) {
     int max_thread = omp_get_max_threads();
     #pragma omp parallel
     {
-        int recursion_depth = (n / max_thread) < max_thread ? max_thread : (n / max_thread);
+        int threshold = (n / (max_thread * max_thread)) < max_thread ? max_thread : (n / (max_thread * max_thread));
         #pragma omp single
-        quicksort(data + 0, data + n-1, recursion_depth);
+        {
+            if (n > 0) quicksort(data + 0, data + n-1, threshold);
+        }
     }
-
-
 }
