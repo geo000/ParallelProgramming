c2258d9cc069885ae84ffff02bc4b6fea844e652
Göncz Levente <gonczl1@pelikaani.org.aalto.fi>
2020-05-17 19:35:51 +0300

    redo matrix multiplication following V2 steps, 9 points

--- cp5/cp.cu
+++ cp5/cp.cu
@@ -13,6 +13,7 @@
 static inline int divup(int a, int b) {
     return (a + b - 1)/b;
 }
+
 static inline int roundup(int a, int b) {
     return divup(a, b) * b;
 }
@@ -26,46 +27,85 @@ static inline void check(cudaError_t err, const char* context) {
 }
 
 #define CHECK(x) check(x, #x)
-#define BLOCK_SIZE 32
 
-__global__ void mykernel(int ny, int nx, int nn_padding, float* norm_data, float* norm_data_transpose, float* result) {
-    if(blockIdx.x > blockIdx.y) return; 
-    __shared__ float norm_data_shared[BLOCK_SIZE][BLOCK_SIZE];
-    __shared__ float norm_data_transpose_shared[BLOCK_SIZE][BLOCK_SIZE];
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    float sum = 0;
+__global__ void mykernel(int ny, int nx, int nn_padding, int n, float* padded_norm_and_transpose, float* result) {
+    if (blockIdx.x > blockIdx.y) return; 
+    int ia = threadIdx.x;
+    int ja = threadIdx.y;
+    int ic = blockIdx.x;
+    int jc = blockIdx.y;
 
-    for (int mozaik_number = 0; mozaik_number < gridDim.x; mozaik_number++)
+    const float* transpose = padded_norm_and_transpose + nn_padding * nn_padding;
+
+    float v[8][8];
+    for (int ib = 0; ib < 8; ib++)
     {
-        int j = mozaik_number * BLOCK_SIZE + threadIdx.x;
-        int i = mozaik_number * BLOCK_SIZE + threadIdx.y;
-
-        norm_data_shared[threadIdx.y][threadIdx.x] = norm_data[row * nn_padding + j];
-        norm_data_transpose_shared[threadIdx.y][threadIdx.x] = norm_data_transpose[i * nn_padding + col];
-        __syncthreads();
-
-        for (int k = 0; k < BLOCK_SIZE; k++)
+        for (int jb = 0; jb < 8; jb++)
         {
-            sum += norm_data_shared[threadIdx.y][k] * norm_data_transpose_shared[k][threadIdx.x];
+            v[ib][jb] = 0;
         }
-        __syncthreads();
     }
-    if (row < ny && col < ny) result[row + col * ny] = sum;
+    for (int k = 0; k < n; k++)
+    {
+        float x[8];
+        float y[8];
+        for (int ib = 0; ib < 8; ib++)
+        {
+            int i = ic * 64 + ib * 8 + ia;
+            x[ib] = transpose[nn_padding * k + i];
+        }
+        for (int jb = 0; jb < 8; jb++)
+        {
+            int j = jc * 64 + jb * 8 + ja;
+            y[jb] = transpose[nn_padding * k + j];
+        }
+        for (int ib = 0; ib < 8; ib++)
+        {
+            for (int jb = 0; jb < 8; jb++)
+            {
+                v[ib][jb] += x[ib] * y[jb];
+            }
+        }
+    }
+    for (int ib = 0; ib < 8; ib++)
+    {
+        for (int jb = 0; jb < 8; jb++)
+        {
+            int i = ic * 64 + ib * 8 + ia;
+            int j = jc * 64 + jb * 8 + ja;
+            if (i < ny && j < ny) result[j + i*ny] = v[ib][jb];
+        }
+    }
 }
 
-__global__ void mykernel_transpose(int nn_padding, float* norm_data, float* norm_data_transpose) {
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    int j = threadIdx.y + blockIdx.y * blockDim.y;
-    norm_data_transpose[i + j * nn_padding] = norm_data[j + i * nn_padding];
+__global__ void mykernel_transpose(int nn_padding, int nx, int ny, float* padded_norm_and_transpose, float* norm_data_temp) {
+    int ja = threadIdx.x;
+    int i = blockIdx.y;
+    float* transpose = padded_norm_and_transpose + nn_padding * nn_padding;
+
+    for (int jb = 0; jb < nn_padding; jb += 64) 
+    {
+        int j = jb + ja;
+        float v = (i < ny && j < nx) ? norm_data_temp[nx * i + j] : 0;
+        padded_norm_and_transpose[nn_padding * i + j] = v;
+        transpose[nn_padding * j + i] = v;
+    }
 }
 
 void correlate(int ny, int nx, const float* data, float* result) {
-    int nn_padding = 0;
-    if (ny >= nx) nn_padding = roundup(ny, BLOCK_SIZE);
-    else nn_padding = roundup(nx, BLOCK_SIZE);
+    int nn_padding = 0, n = 0;
+    if (ny >= nx)
+    {
+        nn_padding = roundup(ny, 64);
+        n = ny;
+    }
+    else
+    {
+        nn_padding = roundup(nx, 64);
+        n = nx;
+    }
 
-    std::vector<float> interm_data(nn_padding * nn_padding);
+    std::vector<float> interm_data(nx * ny);
     for (int row = 0; row < ny ; row++)
     {
         float sum = 0;
@@ -78,56 +118,47 @@ void correlate(int ny, int nx, const float* data, float* result) {
         for (int column = 0; column < nx; column ++)
         {
             float x = data[column + row * nx] - mean;
-            interm_data[column + row * nn_padding] = x;
+            interm_data[column + row * nx] = x;
             square_sum += x * x;
         }
         square_sum = sqrt(square_sum);
         for(int column = 0; column < nx; column++) {
-            interm_data[column + row * nn_padding] /= square_sum;
-        }
-        for (int column = nx; column < nn_padding; column++)
-        {
-            interm_data[column + row * nn_padding] = 0;
+            interm_data[column + row * nx] /= square_sum;
         }
     }
-    for (int row = ny; row < nn_padding; row++)
-    {
-        for (int column = 0; column < nn_padding; column++)
-        {
-            interm_data[column + row * nn_padding] = 0;
-        }
-    }
+
+    const int input_size = nn_padding * nn_padding * sizeof(float);
+    const int original_size = nx * ny * sizeof(float);
+    const int output_size = ny * ny * sizeof(float);
+    float* dGPU = NULL;
+    float* dGPU_temp = NULL;
+    float* rGPU = NULL;
 
     // Allocate memory & copy data to GPU
-    float* dGPU = NULL;
-    float* dGPU_transpose = NULL;
-    float* rGPU = NULL;
-    CHECK(cudaMalloc((void**)&dGPU, nn_padding * nn_padding * sizeof(float)));
-    CHECK(cudaMalloc((void**)&dGPU_transpose, nn_padding * nn_padding * sizeof(float)));
-    CHECK(cudaMalloc((void**)&rGPU, ny * ny * sizeof(float)));
+    CHECK(cudaMalloc((void**)&dGPU, 2 * input_size));
+    CHECK(cudaMalloc((void**)&dGPU_temp, original_size));
+    CHECK(cudaMalloc((void**)&rGPU, output_size));
+    CHECK(cudaMemcpy(dGPU_temp, interm_data.data(), original_size, cudaMemcpyHostToDevice));
 
-    CHECK(cudaMemcpy(dGPU, interm_data.data(), nn_padding * nn_padding * sizeof(float), cudaMemcpyHostToDevice));
-
-    // Run kernel for transpose
+    // Run kernel for transpose and padding
     {
-        dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-        dim3 dimGrid(divup(nn_padding, dimBlock.x), divup(nn_padding, dimBlock.y));
-        mykernel_transpose<<<dimGrid, dimBlock>>>(nn_padding, dGPU, dGPU_transpose);
+        dim3 dimBlock(64, 1);
+        dim3 dimGrid(1, nn_padding);
+        mykernel_transpose<<<dimGrid, dimBlock>>>(nn_padding, nx, ny, dGPU, dGPU_temp);
         CHECK(cudaGetLastError());
     }
 
     // Run kernel for matrix multiplication
     {
-        dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-        dim3 dimGrid(divup(nn_padding, dimBlock.x), divup(nn_padding, dimBlock.y));
-        mykernel<<<dimGrid, dimBlock>>>(ny, nx, nn_padding, dGPU, dGPU_transpose, rGPU);
+        dim3 dimBlock(8, 8);
+        dim3 dimGrid(nn_padding / 64, nn_padding / 64);
+        mykernel<<<dimGrid, dimBlock>>>(ny, nx, nn_padding, n, dGPU, rGPU);
         CHECK(cudaGetLastError());
     }
 
-
     // Copy data back to CPU & release memory
-    CHECK(cudaMemcpy(result, rGPU, ny * ny * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK(cudaMemcpy(result, rGPU, output_size, cudaMemcpyDeviceToHost));
     CHECK(cudaFree(dGPU));
-    CHECK(cudaFree(dGPU_transpose));
+    CHECK(cudaFree(dGPU_temp));
     CHECK(cudaFree(rGPU));
 }
